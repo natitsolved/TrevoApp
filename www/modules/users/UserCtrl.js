4ac59e96545486d2db693daad3c4a75a35e7f930
nits_koushik <nits.koushik@gmail.com>
2017-04-06 14:29:48 +0530

    Commit by Koushik

--- www/modules/users/UserCtrl.js
+++ www/modules/users/UserCtrl.js
@@ -1,4 +1,4 @@
-app.controller('UserCtrl', function ($scope, $stateParams, ionicMaterialInk, $ionicPopup, $timeout, authService, $state,$window,$rootScope) {
+app.controller('UserCtrl', function ($scope, $stateParams, ionicMaterialInk, $ionicPopup, $timeout, authService, $state, $window, $rootScope) {
     //ionic.material.ink.displayEffect();
     ionicMaterialInk.displayEffect();
 
@@ -23,10 +23,10 @@ app.controller('UserCtrl', function ($scope, $stateParams, ionicMaterialInk, $io
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].User_Id != $scope.userInfo.userId) {
                         if (res.includes(data[i].User_Id)) {
-                             obj = { User_Id: data[i].User_Id, Name: data[i].Name, ImagePath: data[i].ImagePath, Icon_Path: data[i].Icon_Path, NativeAbbrv: data[i].NativeAbbrv, LearningAbbrv: data[i].LearningAbbrv, IsOnline: true }
+                            obj = { User_Id: data[i].User_Id, Name: data[i].Name, ImagePath: data[i].ImagePath, Icon_Path: data[i].Icon_Path, NativeAbbrv: data[i].NativeAbbrv, LearningAbbrv: data[i].LearningAbbrv, IsOnline: true }
                         }
                         else {
-                             obj = { User_Id: data[i].User_Id, Name: data[i].Name, ImagePath: data[i].ImagePath, Icon_Path: data[i].Icon_Path, NativeAbbrv: data[i].NativeAbbrv, LearningAbbrv: data[i].LearningAbbrv, IsOnline: false }
+                            obj = { User_Id: data[i].User_Id, Name: data[i].Name, ImagePath: data[i].ImagePath, Icon_Path: data[i].Icon_Path, NativeAbbrv: data[i].NativeAbbrv, LearningAbbrv: data[i].LearningAbbrv, IsOnline: false }
                         }
                         $scope.userList.push(obj);
                     }
@@ -39,11 +39,10 @@ app.controller('UserCtrl', function ($scope, $stateParams, ionicMaterialInk, $io
             });
         });
     };
-   
-
     $scope.userInfo = JSON.parse($window.localStorage["userInfo"]);
     $rootScope.socket = io('http://166.62.40.135:8095', { query: "userId=" + $scope.userInfo.userId });
     console.log($rootScope.socket);
+
     $scope.getUserListing();
     $scope.goToChat = function (userId,name,imagePath)
     {
@@ -52,25 +51,19 @@ app.controller('UserCtrl', function ($scope, $stateParams, ionicMaterialInk, $io
         $state.go('chat', {}, { reload: true });
     }
 
+    $scope.logOut = function () {
+        authService.logout();
+        $state.go('signin', {}, { reload: true });
+    }
+
     $scope.goToChatbot = function () {
         //var userDetails = { userId: userId, name: name, imagePath, imagePath };
         //$window.localStorage["userDetails"] = JSON.stringify(userDetails);
         $state.go('chatbot', {}, { reload: true });
     }
    
-
-
-
-    $scope.doRefresh = function ()
-    {
+    $scope.doRefresh = function () {
         $scope.getUserListing();
         $scope.$broadcast('scroll.refreshComplete');
-    }
-
-
-    $scope.logOut = function ()
-    {
-        authService.logout();
-        $state.go('signin', {}, { reload: true });
     }
 });
